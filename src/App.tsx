bdcde45c2506f2250083c7b1efb164573e2954bc
progriro <progriro@gmail.com>
2020-07-24 23:51:41 +0900

    :tada: Chakra-ui を導入 :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ThemeProvider>
+        <CSSReset />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
